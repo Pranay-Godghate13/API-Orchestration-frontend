b587a57a70b084001ebca0a33259ce8875162935
Pranay <pranaygodghate13@gmail.com>
2025-11-09 19:05:29 +0530

    Loading data & handling error implemented

--- src/App.jsx
+++ src/App.jsx
@@ -7,17 +7,26 @@ function App() {
   
   const [query, setQuery] = useState('');
   const [users, setUsers] = useState([]);
+  const [loading,setLoading]=useState(false);
+  const [error,setError]=useState();
 
   const handleSearch = () => {
     console.log('Searching for:', query);
+    setLoading(true);
     fetch(`http://localhost:8080/user/keyword?keyword=${encodeURIComponent(query)}`)
       .then(response => response.json())
       .then(json => {
-      console.log('Results:', json.content); 
-      setUsers(json.content);                
+        console.log('Results:', json.content); 
+        setUsers(json.content);
+        setLoading(false);                
     })
-      .catch(err => console.error('Search error:', err));
+      .catch((error)=>{
+        console.error(`Error fetching data: `,error);
+        setError('Failed to fetch the data.');
+        setLoading(false);
+      });
   };
+
 
   const handleChange = (e) => {
     const value = e.target.value;
@@ -35,6 +44,14 @@ function App() {
     }
   };
 
+  if(loading)
+  {
+    return <p className='loading-class'>Loading...</p>
+  }
+  if(error)
+  {
+    return <p className='error-class'>{error}</p>
+  }
   
   return (
     <div className="search-container" id="search-container">
@@ -54,25 +71,27 @@ function App() {
         <table className="user-table">
           <thead>
             <tr>
-              <th>User ID</th>
+             
               <th>First Name</th>
               <th>Last Name</th>
               <th>Role</th>
               <th>Age</th>
               <th>Email</th>
               <th>Gender</th>
+              <th>SSN</th>
             </tr>
           </thead>
           <tbody>
             {users.map((user, index) => (
               <tr key={index}>
-                <td>{user.id}</td>
+                
                 <td>{user.firstName}</td>
                 <td>{user.lastName}</td>
                 <td>{user.role}</td>
                 <td>{user.age}</td>
                 <td>{user.email}</td>
                 <td>{user.gender}</td>
+                <td>{user.ssn}</td>
               </tr>
             ))}
           </tbody>
